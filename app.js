43d33890603d0758b0f855bb76cc4e220f1c4b4f
Wojciech Cywinski <wojciech@cywinski.pro>
2017-10-29 12:46:42 +0000

    add new dir views and a landing.ejs template

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req,res){
-    res.send("This will be the landing page soon!");
+    res.render("landing");
+});
+
+app.get("/airlines", function(req,res){
+     var airlines = [
+        {name: "Emirates", image: "http://imgproc.airliners.net/photos/airliners/5/3/6/4645635.jpg?v=v447142f4cac"},
+        {name: "Air Berlin", image: "http://imgproc.airliners.net/photos/airliners/3/8/1/4661183.jpg?v=v418abde9484"},
+        {name: "LOT", image: "http://imgproc.airliners.net/photos/airliners/7/0/6/4487607.jpg?v=v485773631c9"}
+    ];
+    res.render("airlines");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
