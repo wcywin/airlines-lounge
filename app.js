0a8171fa3f6016266a9d86d24568c1eb93f3c50d
Wojciech Cywinski <wojciech@cywinski.pro>
2017-10-31 12:03:18 +0000

    add new dir airlines and comments to separate the new.ejs template, add comment routes get and post

--- app.js
+++ app.js
@@ -6,18 +6,20 @@ var express             = require("express"),
     Comment             = require("./models/comment"),
     seedDB              = require("./seeds");
 
-seedDB();
+
 mongoose.connect("mongodb://localhost/airlines", {useMongoClient: true});
 mongoose.promise = global.promise;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+seedDB();
+
 // var airlines = [
 //         {name: "Emirates", image: "http://imgproc.airliners.net/photos/airliners/5/3/6/4645635.jpg?v=v447142f4cac"},
 //         {name: "Air Berlin", image: "http://imgproc.airliners.net/photos/airliners/3/8/1/4661183.jpg?v=v418abde9484"},
 //         {name: "LOT", image: "http://imgproc.airliners.net/photos/airliners/7/0/6/4487607.jpg?v=v485773631c9"},
-//          {name: "American Airlines", image: "http://imgproc.airliners.net/photos/airliners/5/5/5/4652555.jpg?v=v434a1f3a109"}
+//         {name: "American Airlines", image: "http://imgproc.airliners.net/photos/airliners/5/5/5/4652555.jpg?v=v434a1f3a109"}
 // ];
 
 app.get("/", function(req,res){
@@ -30,7 +32,7 @@ app.get("/airlines", function(req,res){
         if(err){
             console.log(err);
         } else {
-            res.render("index", {airlines:allAirlines});
+            res.render("airlines/index", {airlines:allAirlines});
         }
     });
 });
@@ -53,7 +55,7 @@ app.post("/airlines", function(req,res){
 });
 
 app.get("/airlines/new", function(req, res) {
-    res.render("new"); 
+    res.render("airlines/new"); 
 });
 
 // SHOW - shows more info about the airline
@@ -65,9 +67,46 @@ app.get("/airlines/:id", function(req,res){
         } else {
             console.log(foundAirline);
             //render show template of the airline
-            res.render("show", {airline: foundAirline});
+            res.render("airlines/show", {airline: foundAirline});
         }
     });
+});
+
+// =========================================
+// Comments Routes
+// =========================================
+app.get("/airlines/:id/comments/new", function(req, res) {
+    // find airline by id
+    Airline.findById(req.params.id, function(err, airline){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {airline: airline});
+        }
+    });
+});
+
+app.post("/airlines/:id/comments", function(req,res){
+    // lookup airline using ID
+    Airline.findById(req.params.id, function(err, airline) {
+        if(err){
+            console.log(err);
+            res.redirect("/airlines");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    airline.comments.push(comment);
+                    airline.save();
+                    res.redirect('/airlines/' + airline._id);
+                }
+            });
+        }
+    })
+    // create a new comment
+    // connect new comment to airline
+    // redirect to airline show page
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
