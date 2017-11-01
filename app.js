4e9138b31d4a6fdd2b26d126686bfbfb6a76c1fd
Wojciech Cywinski <wojciech@cywinski.pro>
2017-11-01 21:12:16 +0000

    add login routes and logic, middleware and template

--- app.js
+++ app.js
@@ -147,6 +147,19 @@ app.post("/register", function(req, res) {
     });
 });
 
+// show login form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+// handlign login logic
+app.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/airlines",
+        failureRedirect: "/login"
+    }), function(req, res) {
+});
+
 
 // ====================
 // SERVER CONFIG
